ff6afdc17436b2fbd2964dbed3d367d0a1f323a0
devnazir <nazirbachrululum@gmail.com>
2024-05-22 16:05:42 +0700

    init

---
+++ README.md
@@ -0,0 +1,23 @@
+## WISTIA VIDEO DOWNLOADER
+
+This is a simple script to download videos from Wistia.
+
+### How to use
+
+1. Clone this repository
+2. Install `jq` https://jqlang.github.io/jq/
+3. Copy video ID from the video URL (right-click on the video and select "Copy link and thumbnail")
+4. Paste the video ID in the target.txt (look at the target.example.txt)
+5. Run the script
+
+```bash
+bash script.sh <quality> <topic>
+
+# quality: 1080p, 720p, 540p, 360p
+# topic: the name of the folder where the video will be saved
+# Example: bash script.sh 1080p "Data Structures and Algorithms"
+```
+
+### Disclaimer
+
+This script is for educational purposes only. I am not responsible for any misuse of this script.
---
+++ script.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# Download video from Wistia
+# Usage: bash script.sh <quality> <topic>
+# Example: bash script.sh 1080p "Data Structures and Algorithms"
+
+# Check if the user has provided the quality and topic
+if [ $# -ne 2 ]; then
+    echo "Usage: bash script.sh <quality> <topic>"
+    exit 1
+fi
+
+# Check if topic contains spaces, if contains spaces, replace it with underscore
+if echo "$2" | grep -q ' '; then
+    topic=$(echo $2 | tr ' ' '_')
+else
+    topic=$2
+fi
+
+quality=$1
+target_dir="$HOME/Downloads/$topic"
+
+if [ ! -d "$target_dir" ]; then
+    mkdir -p "$target_dir"
+fi
+
+# Get the video ID from target.txt
+video_ids=$(cat target.txt)
+ 
+for video_id in $video_ids
+do
+    # Get title of the video
+    title=$(curl -s "https://fast.wistia.net/embed/medias/$video_id.json" | jq '.media.name' | cut -d "/" -f 4 | tr -d '"' | cut -d "_" -f 2- | sed -E 's/(\.mp4|\.mov)//g')
+    echo "Downloading video: $title"
+
+    if [ -f "$target_dir/$title.mp4" ]; then
+        echo -e "\e[33mVideo already exists\e[0m\n"
+        continue
+    fi
+
+    # Download the video
+    curl -s "https://fast.wistia.net/embed/iframe/$video_id" \
+    | grep iframeInit \
+    | sed -e 's/W.iframeInit(\(.*\), {});/\1/' \
+    | jq '.assets[] | select(.display_name == "'$quality'").url' \
+    | sed 's/bin/mp4/' \
+    | xargs curl -so "$target_dir/$title.mp4"
+
+    if [ $? -eq 0 ]; then
+        echo -e "\e[32mVideo downloaded successfully\e[0m\n"
+    else
+        echo -e "\e[31mFailed to download the video\e[0m\n"
+    fi
+done
