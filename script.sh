e633040cf8a287726341e2131a4e89bfee66ebdb
devnazir <nazirbachrululum@gmail.com>
2024-05-23 09:53:12 +0700

    feat: add count prefix to the filename

--- script.sh
+++ script.sh
@@ -26,18 +26,21 @@ fi
 
 # Get the video ID from target.txt
 video_ids=$(cat target.txt)
+count=1
  
 for video_id in $video_ids
 do
-    # Get title of the video
-
     json=$(curl -s "https://fast.wistia.net/embed/medias/$video_id.json")
 
+    # Get title of the video
     title=$(echo $json | jq '.media.name' | cut -d "/" -f 4 | tr -d '"' | cut -d "_" -f 2- | sed -E 's/(\.mp4|\.mov)//g')
     echo "Downloading video: $title"
 
-    if [ -f "$target_dir/$title.mp4" ]; then
+    file_name="${count}_${title}.mp4"
+
+    if [ -f "$target_dir/$file_name" ]; then
         echo -e "\e[33mVideo already exists\e[0m\n"
+        count=$((count+1))
         continue
     fi
 
@@ -45,11 +48,13 @@ do
     echo $json \
     | jq '[.media.assets[] | select(.display_name == "'$quality'").url] | first' \
     | sed 's/bin/mp4/' \
-    | xargs curl -s --output "$target_dir/$title.mp4"
+    | xargs curl -s --output "$target_dir/$file_name"
 
     if [ $? -eq 0 ]; then
         echo -e "\e[32mVideo downloaded successfully\e[0m\n"
     else
         echo -e "\e[31mFailed to download the video\e[0m\n"
     fi
+
+    count=$((count+1))
 done
